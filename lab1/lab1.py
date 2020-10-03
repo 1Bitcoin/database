e5aa44ffe341897ad74d27321b84dd28faec07d7
1Bitcoin <shigalkin@mail.ru>
2020-10-03 21:53:07 +0300

    fix lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -46,10 +46,12 @@ def generateProducts():
     f = open('products.csv', 'w')
 
     descriptions = ['good', 'so-so', 'bad', 'very bad', 'very good', 'normal', 'premium', 'premiun lux']
-    name = ['computer', 'car', 'milk', 'game', 'cat', 'dog', 'paper', 'table', 'negr', 'bed', 'air pods', 'book']
+    name = ['computer', 'player', 'teapot', 'monitor', 'HDD', 'SSD', 'videocard', 'RAM', 'CPU', 'motherboard', 'air pods']
+    type_s = ['used', 'new']
+    dimension = ['10x55x45', '100x45x2', '200x100x1','55x77x99', '12x90x56', '45x34x56', '55x77x88', '90x123x1', '111x68x7']
     
     for i in range(MAX_N):
-        line = "{0},{1},{2}\n".format(random.choice(name), random.choice(descriptions), random.randrange(2, 10000))
+        line = "{0},{1},{2},{3},{4}\n".format(random.choice(name), random.choice(descriptions), random.randrange(2, 10000),random.choice(type_s),random.choice(dimension))
         f.write(line)
     f.close()
 
@@ -64,10 +66,12 @@ def generateProductsphoto():
     f.close()
 
 def generateCardproducts():
+    faker = Faker()
     f = open('cardProducts.csv', 'w')
-    
     for i in range(COUNT_BASKETS):
-        line = "{0},{1}\n".format(i + 1, random.randrange(1, 10000))
+        ad = faker.address()
+        ad = ad.replace('\n', ' ')
+        line = f"{i + 1} | {random.randrange(1, 10000)}|{ad}|{random.randrange(1, 3)}|{random.randrange(5, 18)}|{random.randrange(0, 2)}\n"
         f.write(line)
         
     f.close()
